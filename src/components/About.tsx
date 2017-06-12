294c083aeccc9390696a9fc926a3c10bf7135f9e
Matthew Landauer <mlandauer@gmail.com>
2017-06-12 10:34:46 +1000

    Template of an about page

---
+++ src/components/About.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react'
+import { Text } from 'react-native';
+
+// Move this somewhere else (.d.ts)
+// var REVISION: string;
+
+export default class About extends React.Component<{},{}> {
+  render() {
+    return (
+      <Text style={{padding: 30}}>Hello!</Text>
+    )
+  }
+    // var githubURL = "https://github.com/mlandauer/thats-camping-react/commit/" + REVISION
+
+  //   return (
+  //     <div className="about">
+  //       <Header title="About" hideBackButton={false} showAboutButton={false}/>
+  //       <div className="content">
+  //         <div className="container">
+  //           <h2>About That's Camping</h2>
+  //
+  //           <img src="/apple-touch-icon.png" className="img-rounded" />
+  //
+  //           <p>
+  //             Find campsites near you in New South Wales, Australia.
+  //             It covers camping on public, common land such as National Parks,
+  //             State Forests and Local Council land.
+  //           </p>
+  //
+  //           <p>
+  //             It works <strong>completely offline</strong>, even when you're
+  //             far far away from a mobile phone tower. When does that ever happen
+  //             while camping?
+  //           </p>
+  //
+  //           <p>
+  //             Made by <a href="https://twitter.com/matthewlandauer">Matthew Landauer</a> based on an iOS app by Matthew and <a href="https://twitter.com/katska">Katherine Szuminska</a>. It's free and <a href="https://github.com/mlandauer/thats-camping-react">open source</a> because that's the way it ought to be.
+  //           </p>
+  //
+  //           <p>
+  //             You're currently using version <a href={githubURL}>{REVISION}</a>.
+  //           </p>
+  //
+  //           <h2>Things you might want to do</h2>
+  //
+  //           <p>
+  //             <a href="https://github.com/mlandauer/thats-camping-react/issues">
+  //               Suggest a <strong>feature</strong> or report an <strong>issue</strong>
+  //             </a>
+  //           </p>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   )
+  // }
+}
--- src/index.ios.tsx
+++ src/index.ios.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { AppRegistry } from 'react-native';
 import CampsiteList from './components/CampsiteList'
+import About from './components/About'
 
 export default class ThatsCamping extends React.Component<{}, {}> {
   render() {
     return (
-      <CampsiteList/>
+      <About/>
     );
   }
 }
