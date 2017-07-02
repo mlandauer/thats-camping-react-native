dab0fe7ec316148677439031e8ec1644476fb00a
Matthew Landauer <matthew@studiothick.com>
2017-07-02 12:46:20 +1000

    Show which park a campsite is in

--- src/screens/CampsiteDetailScreen.tsx
+++ src/screens/CampsiteDetailScreen.tsx
@@ -12,6 +12,7 @@ export default class CampsiteDetailScreen extends React.Component<Props, {}> {
     return (
       <View style={styles.container}>
         <Text style={styles.heading}>{this.props.campsite.name}</Text>
+        <Text style={styles.park}>In {this.props.campsite.parkName}</Text>
         <Text style={styles.description}>{this.props.campsite.description}</Text>
       </View>
     )
@@ -25,7 +26,11 @@ const styles = StyleSheet.create({
   heading: {
     fontWeight: 'bold' as 'bold',
     fontSize: 20,
-    marginBottom: 20
+    marginBottom: 10
+  },
+  park: {
+    fontSize: 20,
+    marginBottom: 10
   },
   description: {
     fontSize: 20
