21618aafcd7daaf71df5bca586f2ff4b82bde0bc
Matthew Landauer <matthew@studiothick.com>
2017-07-05 04:06:01 +1000

    Naive type definition for react-native-button

---
+++ src/react-native-button.d.ts
@@ -0,0 +1,13 @@
+declare module "react-native-button" {
+  import { ViewStyle, TextStyle } from 'react-native'
+
+  // TODO: Add all of the supported props
+  interface Props {
+    containerStyle: ViewStyle;
+    style: TextStyle;
+    onPress: () => void;
+  }
+
+  export default class Button extends React.Component<Props, {}> {
+  }
+}
