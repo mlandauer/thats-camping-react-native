a5028509575f8976fbe93a366dd747665693023f
Matthew Landauer <mlandauer@gmail.com>
2017-07-12 03:40:01 +1000

    Persist starred campsites

--- src/index.ios.tsx
+++ src/index.ios.tsx
@@ -1,19 +1,31 @@
 import { Navigation } from 'react-native-navigation';
+import { AsyncStorage } from 'react-native'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
+import {persistStore, autoRehydrate} from 'redux-persist'
 
 import { registerScreens } from './screens';
 import { reducer, State } from './ducks'
 import * as CampsitesActions from './ducks/campsites'
 import * as PositionActions from './ducks/position'
 
-const store = createStore(reducer, {
-  campsites: {},
-  // TODO: Would be better if this could be undefined
-  position: null,
-  starred: []
-}, applyMiddleware(thunk))
+const store = createStore(
+  reducer,
+  {
+    campsites: {},
+    // TODO: Would be better if this could be undefined
+    position: null,
+    starred: []
+  },
+  compose(
+    applyMiddleware(thunk),
+    autoRehydrate()
+  )
+)
+
+// begin periodically persisting part of the store (just the starred campsites)
+persistStore(store, {storage: AsyncStorage, whitelist: ['starred']})
 
 // Immediately start getting the campsites data and location
 var json = require('../data_simplified.json')
