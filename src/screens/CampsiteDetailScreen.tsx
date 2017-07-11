ab82ac5d4ead81f5916c7326206b5e77cef03683
Matthew Landauer <mlandauer@gmail.com>
2017-07-12 03:51:16 +1000

    Inline component once more

--- src/screens/CampsiteDetailScreen.tsx
+++ src/screens/CampsiteDetailScreen.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react'
+import {
+  ScrollView,
+  Text,
+  StyleSheet,
+  Linking,
+  View
+} from 'react-native'
 import { connect, Dispatch } from 'react-redux'
+import Button from 'react-native-button'
 
-import { Campsite, CampsiteWithStarred } from '../libs/types'
-import CampsiteDetail from '../components/CampsiteDetail'
+import { Campsite, CampsiteWithStarred, Position } from '../libs/types'
 import { State } from '../ducks'
 import { toggleStarredCampsite } from '../ducks/starred'
 import convertToCampsiteWithStarred from '../libs/convertToCampsiteWithStarred'
+import Star from '../components/Star'
+import * as TextFormatter from '../libs/TextFormatter'
 
 interface Props {
   campsite: CampsiteWithStarred;
@@ -17,10 +26,103 @@ interface Props {
 export class CampsiteDetailScreen extends React.Component<Props, {}> {
   render() {
     return (
-      <CampsiteDetail campsite={this.props.campsite} onStarToggled={this.props.onStarToggled} />
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+            <View style={{flex: 1}}>
+              <Text style={styles.heading}>{this.props.campsite.name}</Text>
+              <Text style={styles.park}>{this.props.campsite.parkName}</Text>
+            </View>
+            <Button onPress={this.props.onStarToggled}>
+              <Star starred={this.props.campsite.starred} />
+            </Button>
+          </View>
+          <DescriptionText description={this.props.campsite.description}/>
+          <Text style={styles.facilities}>Facilities</Text>
+          <Text style={styles.description}>{TextFormatter.facilitiesText(this.props.campsite.facilities)}</Text>
+          <Text style={styles.access}>Access</Text>
+          <Text style={styles.description}>{TextFormatter.accessText(this.props.campsite.access)}</Text>
+          <Button
+            containerStyle={styles.buttonContainer}
+            style={styles.buttonText}
+            onPress={() => {this.onPress()}}>
+            Directions to campsite
+          </Button>
+        </View>
+      </ScrollView>
+    )
+  }
+
+  // TODO: Disable button when appropriate
+  onPress() {
+    let url = mapUrl(this.props.campsite.position)
+    if (url != undefined) {
+      Linking.openURL(url)
+    }
+  }
+}
+
+function DescriptionText(props: {description: string}) {
+  if (props.description == "") {
+    return null
+  } else {
+    return (
+      <Text style={styles.description}>{props.description}</Text>
     )
   }
 }
+
+function mapUrl(position: Position | null): string | undefined {
+  if (position == null) {
+    return undefined;
+  } else {
+    return "https://maps.google.com/maps?" +
+      "daddr=" +
+      position.lat + "," + position.lng;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+   padding: 20
+  },
+  heading: {
+    fontWeight: 'bold' as 'bold',
+    fontSize: 20,
+    flexShrink: 0.6
+  },
+  park: {
+    fontSize: 20,
+    color: '#aaa',
+    marginBottom: 20
+  },
+  description: {
+    fontSize: 20,
+    marginBottom: 20
+  },
+  facilities: {
+    fontWeight: 'bold' as 'bold',
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  access: {
+    fontWeight: 'bold' as 'bold',
+    fontSize: 20,
+    marginBottom: 10
+  },
+  buttonContainer: {
+    padding: 10,
+    marginTop: 20,
+    height: 45,
+    overflow: 'hidden',
+    borderRadius: 4,
+    borderWidth: 0.5
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#777'
+  }
+})
 
 function mapStateToProps(state: State, ownProps: {id: number}) {
   let campsite = convertToCampsiteWithStarred(state.campsites[ownProps.id], state.starred)
