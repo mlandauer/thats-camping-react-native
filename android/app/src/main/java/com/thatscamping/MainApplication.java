1611d4396e402628623ee2abf7d6a7b9267d7a7e
Matthew Landauer <mlandauer@gmail.com>
2017-07-18 17:04:51 +1000

    Add stetho for android debugging

--- android/app/src/main/java/com/thatscamping/MainApplication.java
+++ android/app/src/main/java/com/thatscamping/MainApplication.java
@@ -3,6 +3,12 @@ package com.thatscamping;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.modules.network.ReactCookieJarContainer;
+import com.facebook.stetho.Stetho;
+import okhttp3.OkHttpClient;
+import com.facebook.react.modules.network.OkHttpClientProvider;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+import java.util.concurrent.TimeUnit;
 
 import java.util.Arrays;
 import java.util.List;
@@ -29,5 +35,18 @@ public class MainApplication extends NavigationApplication {
     @Override
     public List<ReactPackage> createAdditionalReactPackages() {
         return getPackages();
+    }
+
+    public void onCreate() {
+      super.onCreate();
+      Stetho.initializeWithDefaults(this);
+      OkHttpClient client = new OkHttpClient.Builder()
+      .connectTimeout(0, TimeUnit.MILLISECONDS)
+      .readTimeout(0, TimeUnit.MILLISECONDS)
+      .writeTimeout(0, TimeUnit.MILLISECONDS)
+      .cookieJar(new ReactCookieJarContainer())
+      .addNetworkInterceptor(new StethoInterceptor())
+      .build();
+      OkHttpClientProvider.replaceOkHttpClient(client);
     }
 }
