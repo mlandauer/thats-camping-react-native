f37d38c884ec031aaf4ff9bbab0da53b0f80a1ae
Matthew Landauer <mlandauer@gmail.com>
2017-07-25 03:29:02 +1000

    Extract handling of about button

--- src/screens/CampsiteMapScreen.tsx
+++ src/screens/CampsiteMapScreen.tsx
@@ -2,8 +2,7 @@ import * as React from 'react'
 import { connect, Dispatch } from 'react-redux'
 import { View, Dimensions } from 'react-native'
 import CampsiteMap from '../components/CampsiteMap'
-import { Event, Navigator } from 'react-native-navigation'
-import Icon from 'react-native-vector-icons/Ionicons'
+import { Navigator } from 'react-native-navigation'
 
 import {
   CampsiteWithStarred,
@@ -12,6 +11,7 @@ import {
 import { State } from '../ducks'
 import shortenName from '../libs/shortenName'
 import convertToCampsiteWithStarred from '../libs/convertToCampsiteWithStarred'
+import ScreenWithAbout from './ScreenWithAbout'
 
 interface Props {
   navigator?: Navigator;
@@ -19,46 +19,7 @@ interface Props {
   position: Position | null;
 }
 
-export class CampsiteMapScreen extends React.Component<Props, {}> {
-  constructor(props: Props) {
-    super(props);
-    // The navigator prop isn't necessarily set when we run tests
-    if (this.props.navigator) {
-      this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
-    }
-  }
-
-  componentDidMount() {
-    // HACK HACK Temporary workaround for not figuring out how to mock out Icon for testing
-    // Just set it to undefined for the time being during testing
-    if (Icon) {
-      // TODO: Use a different icon on Android
-      Icon.getImageSource('ios-help-circle-outline', 22).then((about: any) => {
-        if (this.props.navigator) {
-          this.props.navigator.setButtons({
-            rightButtons: [
-              { id: 'about', icon: about }
-            ]
-          })
-        }
-      })
-    }
-  }
-
-  onNavigatorEvent(event: Event) {
-    if (event.type == 'NavBarButtonPress') {
-      if (event.id == 'about') {
-        if (this.props.navigator) {
-          this.props.navigator.push({
-            screen: 'thatscamping.AboutScreen',
-            title: 'About',
-            backButtonTitle: 'Back'
-          });
-        }
-      }
-    }
-  }
-
+export class CampsiteMapScreen extends ScreenWithAbout<Props, {}> {
   onPress(id: number) {
     if (this.props.navigator) {
       var campsite: CampsiteWithStarred = this.props.campsites[id]
---
+++ src/screens/ScreenWithAbout.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react'
+import { Event, Navigator } from 'react-native-navigation'
+import Icon from 'react-native-vector-icons/Ionicons'
+
+interface NProps {
+  navigator?: Navigator;
+}
+
+export default class ScreenWithAbout<U,V> extends React.Component<U & NProps,V> {
+  constructor(props: U & NProps) {
+    super(props);
+    // The navigator prop isn't necessarily set when we run tests
+    if (this.props.navigator) {
+      this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
+    }
+  }
+
+  componentDidMount() {
+    // HACK HACK Temporary workaround for not figuring out how to mock out Icon for testing
+    // Just set it to undefined for the time being during testing
+    if (Icon) {
+      // TODO: Use a different icon on Android
+      Icon.getImageSource('ios-help-circle-outline', 22).then((about: any) => {
+        if (this.props.navigator) {
+          this.props.navigator.setButtons({
+            rightButtons: [
+              { id: 'about', icon: about }
+            ]
+          })
+        }
+      })
+    }
+  }
+
+  onNavigatorEvent(event: Event) {
+    if (event.type == 'NavBarButtonPress') {
+      if (event.id == 'about') {
+        if (this.props.navigator) {
+          this.props.navigator.push({
+            screen: 'thatscamping.AboutScreen',
+            title: 'About',
+            backButtonTitle: 'Back'
+          });
+        }
+      }
+    }
+  }
+}
