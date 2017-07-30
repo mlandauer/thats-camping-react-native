52df835945b5459fed44ec100efe283cc8f2b15f
Matthew Landauer <mlandauer@gmail.com>
2017-07-30 10:59:58 +1000

    Extract method

--- src/ducks/campsites.ts
+++ src/ducks/campsites.ts
@@ -17,21 +17,31 @@ export interface CampsitesState {
   readonly [index: number]: Campsite
 };
 
+// Convert the json as stored in data_simplified.json to a list of campsites
+function convertJson(json: CampsitesJson): Campsite[] {
+  // Turn parks array into hash
+  let parksHash: {[index: number]: any} = {}
+  json.parks.forEach((park) => {
+    parksHash[park.id] = park
+  })
+  // Turn array in campsites into hash
+  let c: Campsite[] = []
+  json.campsites.forEach((campsite) => {
+    let park = parksHash[campsite.park_id]
+    // Convert weird representation of undefined position in json to how we should do it
+    let position : (Position | null) = convertPosition(campsite.position)
+    c.push(Object.assign({}, campsite, {parkName: park.name, position: position}))
+  })
+  return c
+}
+
 export default function reducer(state: CampsitesState = {}, action: CampsitesAction): CampsitesState {
   switch(action.type) {
     case 'ADD_CAMPSITES_JSON':
-      // Turn parks array into hash
-      let parksHash: {[index: number]: any} = {}
-      action.json.parks.forEach((park) => {
-        parksHash[park.id] = park
-      })
-      // Turn array in campsites into hash
+      let c2 = convertJson(action.json)
       let c: {[index: number]: Campsite} = {}
-      action.json.campsites.forEach((campsite) => {
-        let park = parksHash[campsite.park_id]
-        // Convert weird representation of undefined position in json to how we should do it
-        let position : (Position | null) = convertPosition(campsite.position)
-        c[campsite.id] = Object.assign({}, campsite, {parkName: park.name, position: position})
+      c2.forEach(campsite => {
+        c[campsite.id] = campsite
       })
       return Object.assign({}, state, c)
     default:
