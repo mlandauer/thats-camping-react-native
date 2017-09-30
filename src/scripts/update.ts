846b7342f3bd4748a417c847add4d16347162f01
Matthew Landauer <mlandauer@gmail.com>
2017-10-01 05:27:19 +1100

    Let's just do it with 2 records to start with

--- src/scripts/update.ts
+++ src/scripts/update.ts
@@ -17,7 +17,7 @@ Dotenv.config()
 async function getMorphData(scraper: string) {
   let s = querystring.stringify({
     key: process.env.MORPH_API_KEY,
-    query: 'select * from "data"'
+    query: 'select * from "data" limit 2'
   })
   let r = await fetch('https://api.morph.io/' + scraper + '/data.json?' + s)
   return r.json()
