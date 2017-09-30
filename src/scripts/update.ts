973a664e8ea6dae02b5b16c9b041fe821d6754ac
Matthew Landauer <mlandauer@gmail.com>
2017-09-30 16:05:26 +1000

    Use new function

--- src/scripts/update.ts
+++ src/scripts/update.ts
@@ -6,30 +6,30 @@ import * as PouchDB from 'pouchdb-node'
 // We're using dotenv to do what we were doing with react-native-config
 import * as Dotenv from 'dotenv'
 
+import { remoteDbCreate } from '../libs/DatabaseGeneric'
+
 // Loads the environment variables from .env
 Dotenv.config()
 
 let db = new PouchDB('./thatscamping.db')
 
-// This is currently copied from Database.ts
-// TODO: Fix this so that it's not copied
-let remoteDb = new PouchDB('https://mlandauer.cloudant.com/thats-camping-react-native', {
-  auth: {
-    username: 'chookeementootworsenters',
-    // Obviously anyone who really wants to get access to the password below
-    // can just decompile the binary. Not including the password in the source
-    // code provides a minimal level of security.
-    password: process.env.COUCHDB_REMOTE_PASSWORD
-  }
-});
+// Obviously anyone who really wants to get access to the password below
+// can just decompile the binary. Not including the password in the source
+// code provides a minimal level of security.
+let password = process.env.COUCHDB_REMOTE_PASSWORD
+if (password) {
+  let remoteDb = remoteDbCreate(password)
 
-// Do a one time of remote to local database
-console.log("Doing replication from remote to local database...")
-PouchDB.replicate(remoteDb, db).then(() => {
-  console.log("Replication finished")
+  // Do a one time of remote to local database
+  console.log("Doing replication from remote to local database...")
+  PouchDB.replicate(remoteDb, db).then(() => {
+    console.log("Replication finished")
 
-  // Just spit out the contents of the database to the standard output
-  db.allDocs({include_docs: true}).then((docs) => {
-    console.log(docs.rows)
+    // Just spit out the contents of the database to the standard output
+    db.allDocs({include_docs: true}).then((docs) => {
+      console.log(docs.rows)
+    })
   })
-})
+} else {
+  console.error("environment variable COUCHDB_REMOTE_PASSWORD not set")
+}
