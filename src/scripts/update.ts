8a0f3d2370ab13e1283daf52ea5a7dd9d590a52b
Matthew Landauer <mlandauer@gmail.com>
2017-10-01 05:24:50 +1100

    Reinstate the initial replication

--- src/scripts/update.ts
+++ src/scripts/update.ts
@@ -9,33 +9,10 @@ import fetch from 'node-fetch'
 import * as querystring from 'querystring'
 
 import { CampsiteNoId, Campsite, Position } from '../libs/types'
-// import { remoteDbCreate } from '../libs/DatabaseGeneric'
+import { remoteDbCreate } from '../libs/DatabaseGeneric'
 
 // Loads the environment variables from .env
 Dotenv.config()
-
-let db = new PouchDB<CampsiteNoId>('./thatscamping.db')
-
-// Obviously anyone who really wants to get access to the password below
-// can just decompile the binary. Not including the password in the source
-// code provides a minimal level of security.
-// let password = process.env.COUCHDB_REMOTE_PASSWORD
-// if (password) {
-//   let remoteDb = remoteDbCreate(PouchDB, password)
-//
-//   // Do a one time of remote to local database
-//   console.log("Doing replication from remote to local database...")
-//   PouchDB.replicate(remoteDb, db).then(() => {
-//     console.log("Replication finished")
-//
-//     // // Just spit out the contents of the database to the standard output
-//     db.allDocs({include_docs: true}).then((docs) => {
-//       console.log(docs.rows)
-//     })
-//   })
-// } else {
-//   console.error("environment variable COUCHDB_REMOTE_PASSWORD not set")
-// }
 
 async function getMorphData(scraper: string) {
   let s = querystring.stringify({
@@ -114,41 +91,59 @@ async function campsitesFromMorph() {
   return json.map((c) => convertMorphRecordToCampsite(c))
 }
 
-Promise.all([
-  campsitesFromSource('nationalparks.nsw.gov.au'),
-  campsitesFromMorph()
-]).then((results) => {
-  let campsitesSource = results[0]
-  let campsitesMorph = results[1]
+let db = new PouchDB<CampsiteNoId>('./thatscamping.db')
 
-  let docs: (Campsite | CampsiteNoId)[] = []
+// Obviously anyone who really wants to get access to the password below
+// can just decompile the binary. Not including the password in the source
+// code provides a minimal level of security.
+let password = process.env.COUCHDB_REMOTE_PASSWORD
+if (password) {
+  let remoteDb = remoteDbCreate(PouchDB, password)
 
-  // This bit creates new campsites and updates existing ones
-  campsitesMorph.forEach((c) => {
-    let source = campsitesSource.find(campsite => campsite.sourceId === c.sourceId)
-    if (source) {
-      // This is a campsite that might need updating
-      // Need to add the _id and _rev (from source) into the morph data
-      let updated = {...c, _id: source._id, _rev: source._rev}
-      if (JSON.stringify(source) !== JSON.stringify(updated)) {
-        docs.push(updated)
-      }
-    } else {
-      // This is a new campsite
-      docs.push(c)
-    }
+  // Do a one time of remote to local database
+  console.log("Doing replication from remote to local database...")
+  PouchDB.replicate(remoteDb, db).then(() => {
+    console.log("Replication finished")
+
+    Promise.all([
+      campsitesFromSource('nationalparks.nsw.gov.au'),
+      campsitesFromMorph()
+    ]).then((results) => {
+      let campsitesSource = results[0]
+      let campsitesMorph = results[1]
+
+      let docs: (Campsite | CampsiteNoId)[] = []
+
+      // This bit creates new campsites and updates existing ones
+      campsitesMorph.forEach((c) => {
+        let source = campsitesSource.find(campsite => campsite.sourceId === c.sourceId)
+        if (source) {
+          // This is a campsite that might need updating
+          // Need to add the _id and _rev (from source) into the morph data
+          let updated = {...c, _id: source._id, _rev: source._rev}
+          if (JSON.stringify(source) !== JSON.stringify(updated)) {
+            docs.push(updated)
+          }
+        } else {
+          // This is a new campsite
+          docs.push(c)
+        }
+      })
+
+      // This removes campsites
+      campsitesSource.forEach((c) => {
+        let morph = campsitesMorph.find(campsite => campsite.sourceId === c.sourceId)
+        if (!morph) {
+          let updated = {...c, _deleted: true}
+          docs.push(updated)
+        }
+      })
+      console.log("Updating", docs.length, "campsites")
+      db.bulkDocs(docs)
+    }).then(() => {
+      console.log("Database updated")
+    })
   })
-
-  // This removes campsites
-  campsitesSource.forEach((c) => {
-    let morph = campsitesMorph.find(campsite => campsite.sourceId === c.sourceId)
-    if (!morph) {
-      let updated = {...c, _deleted: true}
-      docs.push(updated)
-    }
-  })
-  console.log("Updating", docs.length, "campsites")
-  db.bulkDocs(docs)
-}).then(() => {
-  console.log("Database updated")
-})
+} else {
+  console.error("environment variable COUCHDB_REMOTE_PASSWORD not set")
+}
