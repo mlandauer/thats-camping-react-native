5d6282e8e0d48a3e9f4804a146576830fe12e546
Matthew Landauer <mlandauer@gmail.com>
2017-10-10 03:06:29 +1100

    Take a snapshot

--- ios/ThatsCampingUITests/ThatsCampingUITests.swift
+++ ios/ThatsCampingUITests/ThatsCampingUITests.swift
@@ -33,6 +33,7 @@ class ThatsCampingUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        snapshot("0Launch")
     }
     
 }
