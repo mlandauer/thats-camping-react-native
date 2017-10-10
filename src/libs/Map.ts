5a7da61308b695f737842cfdb4c3e86e4e05eafd
Matthew Landauer <mlandauer@gmail.com>
2017-10-10 11:47:11 +1100

    Get building with new map at least

--- src/libs/Map.ts
+++ src/libs/Map.ts
@@ -23,7 +23,7 @@ interface MaxAllowedTilesPayload {
 export function initialise(updateProgress: (progress: number) => void) {
   Mapbox.setAccessToken(Config.MAPBOX_ACCESS_TOKEN)
   // The API call below is new in react-native-mapbox-gl 5.2.1. We're on 5.2.0
-  // Mapbox.initializeOfflinePacks()
+  Mapbox.initializeOfflinePacks()
   // Get information about all the offline packs currently defined
   Mapbox.getOfflinePacks()
   .then((packs: DownloadProgress[]) => {
