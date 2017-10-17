43d30c612949114b9199867111789968018bc57b
Matthew Landauer <mlandauer@gmail.com>
2017-10-17 14:11:01 +1100

    Extract function

--- src/scripts/google.ts
+++ src/scripts/google.ts
@@ -5,10 +5,6 @@ import * as csvParse from 'csv-parse'
 // Loads the environment variables from .env
 Dotenv.config()
 
-// Google sheet that we want here has to be publically readable
-let google_sheet_id = process.env.GOOGLE_SHEET_ID
-let url = `https://docs.google.com/spreadsheets/d/${google_sheet_id}/export?format=csv`
-
 function parse(doc: string, options: any) {
   return new Promise(function (fulfill, reject) {
     csvParse(doc, options, function(err, output) {
@@ -21,9 +17,20 @@ function parse(doc: string, options: any) {
   })
 }
 
-fetch(url)
-  .then(doc => doc.text())
-  .then(doc => parse(doc, {columns: true}))
-  .then(output => {
+function getPublicGoogleSheetData(google_sheet_id: string) {
+  let url = `https://docs.google.com/spreadsheets/d/${google_sheet_id}/export?format=csv`
+  return fetch(url)
+    .then(doc => doc.text())
+    .then(doc => parse(doc, {columns: true}))
+}
+
+let googleSheetID = process.env.GOOGLE_SHEET_ID
+
+if (googleSheetID) {
+  // Google sheet that we want here has to be publically readable
+  getPublicGoogleSheetData(googleSheetID).then(output => {
     console.log("output", output)
   })
+} else {
+  console.error("Need to set GOOGLE_SHEET_ID in .env")
+}
