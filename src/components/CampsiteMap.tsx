ee923824dd42a29011dacc27d0e255c077f2c70a
Matthew Landauer <mlandauer@gmail.com>
2017-11-08 07:34:47 +1100

    WIP: Moving over to new version of mapping library

--- src/components/CampsiteMap.tsx
+++ src/components/CampsiteMap.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react'
 import {
-  View
+  View,
+  Image,
 } from 'react-native'
-import { MapView } from '@mapbox/react-native-mapbox-gl'
+import MapboxGL from '@mapbox/react-native-mapbox-gl'
 
 import { CampsiteWithStarred } from '../libs/types'
 import shortenName from '../libs/shortenName'
 import OfflineMapControls from './OfflineMapControls'
-import * as Map from '../libs/Map'
+// import * as Map from '../libs/Map'
 import Activity from './Activity'
 
 interface Props {
@@ -19,79 +20,112 @@ interface Props {
   onUpdateProgress: (progress: number) => void;
 }
 
-interface RightAnnotationTappedInfo {
-  id: string;
-  title: null | string;
-  subtitle: null | string;
-  latitude: number;
-  longitude: number;
-}
+// interface RightAnnotationTappedInfo {
+//   id: string;
+//   title: null | string;
+//   subtitle: null | string;
+//   latitude: number;
+//   longitude: number;
+// }
 
 export default class CampsiteMap extends React.Component<Props, {}> {
-  finishLoading() {
-    Map.initialiseOffline(progress => this.props.onUpdateProgress(progress))
-  }
+  // finishLoading() {
+  //   Map.initialiseOffline(progress => this.props.onUpdateProgress(progress))
+  // }
 
   render() {
     // Centering on the "Heartbreak Hill camping area"
     // to start with
-    let initialCenterCoordinate = {
-      latitude: -33.2709845533332,
-      longitude: 150.897112779999
-    }
+    let initialCenterCoordinate = [
+      150.897112779999, -33.2709845533332
+    ]
 
     return (
       <View style={{flex: 1}}>
         <Activity active={this.props.campsites.length == 0 || this.props.replicating} />
-        <MapView
-          onFinishLoadingMap={() => this.finishLoading()}
-          style={{ flex: 1 }}
+        <MapboxGL.MapView
           styleURL="mapbox://styles/mapbox/outdoors-v10"
-          showsUserLocation={true}
+          showUserLocation={true}
           rotateEnabled={false}
           pitchEnabled={false}
-          initialCenterCoordinate={initialCenterCoordinate}
-          initialZoomLevel={7}
-          logoIsHidden={true}
-          annotations={annotations(this.props.campsites)}
-          onRightAnnotationTapped={(info: RightAnnotationTappedInfo) => this.props.onPress(info.id)}
-        />
+          centerCoordinate={initialCenterCoordinate}
+          zoomLevel={7}
+          logoEnabled={false}
+        >
+          <Annotations campsites={this.props.campsites}/>
+        </MapboxGL.MapView>
+        {
+          // <MapView
+          //   onFinishLoadingMap={() => this.finishLoading()}
+          //   style={{ flex: 1 }}
+          //   styleURL="mapbox://styles/mapbox/outdoors-v10"
+          //   showsUserLocation={true}
+          //   rotateEnabled={false}
+          //   pitchEnabled={false}
+          //   initialCenterCoordinate={initialCenterCoordinate}
+          //   initialZoomLevel={7}
+          //   logoIsHidden={true}
+          //   annotations={annotations(this.props.campsites)}
+          //   onRightAnnotationTapped={(info: RightAnnotationTappedInfo) => this.props.onPress(info.id)}
+          // />
+        }
         <OfflineMapControls progress={this.props.downloadProgress} />
       </View>
     )
   }
 }
 
-function annotations(campsites: CampsiteWithStarred[]) {
-  return campsites
-    .filter(campsite => annotation(campsite))
-    .map(campsite => annotation(campsite))
+function Annotations(props: {campsites: CampsiteWithStarred[]}) {
+  return props.campsites
+    .filter(campsite => campsite.position)
+    .map(campsite => <Annotation campsite={campsite} key={campsite._id}/>)
 }
 
-function annotation(campsite: CampsiteWithStarred) {
-  if (campsite.position) {
-    return {
-      coordinates: [campsite.position.lat, campsite.position.lng],
-      type: 'point',
-      title: shortenName(campsite.name),
-      subtitle: shortenName(campsite.parkName),
-      id: campsite._id,
-      annotationImage: {
-        source: {
-          uri: (campsite.starred ? 'starred_map_pin' : 'default_map_pin')
-        },
-        height: 82.5,
-        width: 29.6
-      },
-      rightCalloutAccessory: {
-        source: {
-          uri: 'ion-ios-arrow-forward'
-        },
-        height: 20,
-        width: 20,
-      }
-    }
+function Annotation(props: {campsite: CampsiteWithStarred}) {
+  if (props.campsite.position) {
+    return (
+      <MapboxGL.PointAnnotation
+        id={props.campsite._id}
+        title={shortenName(props.campsite.name)}
+        coordinate={[ props.campsite.position.lng, props.campsite.position.lat ]}
+      >
+        <Image
+          style={{height: 82.5, width: 29.6}}
+          source={{
+            uri: (props.campsite.starred ? 'starred_map_pin' : 'default_map_pin')}
+          }
+        />
+        <MapboxGL.Callout title={shortenName(props.campsite.name)}/>
+      </MapboxGL.PointAnnotation>
+    )
   } else {
     return null
   }
 }
+// function annotation(campsite: CampsiteWithStarred) {
+//   if (campsite.position) {
+//     return {
+//       coordinates: [campsite.position.lat, campsite.position.lng],
+//       type: 'point',
+//       title: shortenName(campsite.name),
+//       subtitle: shortenName(campsite.parkName),
+//       id: campsite._id,
+//       annotationImage: {
+//         source: {
+//           uri: (campsite.starred ? 'starred_map_pin' : 'default_map_pin')
+//         },
+//         height: 82.5,
+//         width: 29.6
+//       },
+//       rightCalloutAccessory: {
+//         source: {
+//           uri: 'ion-ios-arrow-forward'
+//         },
+//         height: 20,
+//         width: 20,
+//       }
+//     }
+//   } else {
+//     return null
+//   }
+// }
--- src/libs/Map.ts
+++ src/libs/Map.ts
@@ -1,59 +1,59 @@
-import * as Mapbox from '@mapbox/react-native-mapbox-gl'
+import MapboxGL from '@mapbox/react-native-mapbox-gl'
 import Config from 'react-native-config'
 
-interface DownloadProgress {
-  name: string;
-  metadata: any;
-  countOfResourcesCompleted: number;
-  maximumResourcesExpected: number;
-  countOfResourcesExpected: number;
-  countOfBytesCompleted: number;
-}
+// interface DownloadProgress {
+//   name: string;
+//   metadata: any;
+//   countOfResourcesCompleted: number;
+//   maximumResourcesExpected: number;
+//   countOfResourcesExpected: number;
+//   countOfBytesCompleted: number;
+// }
 
 export async function initialise()
 {
-  Mapbox.setAccessToken(Config.MAPBOX_ACCESS_TOKEN)
+  MapboxGL.setAccessToken(Config.MAPBOX_ACCESS_TOKEN)
 }
 
-export async function initialiseOffline( updateProgress: (progress: number) => void) {
-  await Mapbox.initializeOfflinePacks()
-  let packs: DownloadProgress[] = await Mapbox.getOfflinePacks()
-  if (packs.length > 0) {
-    console.log("Already a download pack setup so we don't need to set one up")
-    // Checks if the pack hasn't finished downloading
-    if (progress(packs[0]) < 1) {
-      setupUpdateProgressCallback(updateProgress)
-    }
-  } else {
-    console.log("Need to setup a download pack")
-    setupDownloadPack()
-    setupUpdateProgressCallback(updateProgress)
-  }
+export async function initialiseOffline( _updateProgress: (progress: number) => void) {
+  // await Mapbox.initializeOfflinePacks()
+  // let packs: DownloadProgress[] = await Mapbox.getOfflinePacks()
+  // if (packs.length > 0) {
+  //   console.log("Already a download pack setup so we don't need to set one up")
+  //   // Checks if the pack hasn't finished downloading
+  //   if (progress(packs[0]) < 1) {
+  //     setupUpdateProgressCallback(updateProgress)
+  //   }
+  // } else {
+  //   console.log("Need to setup a download pack")
+  //   setupDownloadPack()
+  //   setupUpdateProgressCallback(updateProgress)
+  // }
 }
 
-function setupDownloadPack() {
-  // Start downloading offline maps
-  Mapbox.addOfflinePack({
-    name: 'base',
-    type: 'bbox',
-    bounds: [
-      -37.845485, 140.913340, -28.126389, 153.402606
-    ],
-    minZoomLevel: 5,
-    maxZoomLevel: 10,
-    // TODO: Get styleURL from configuration
-    styleURL: "mapbox://styles/mapbox/outdoors-v10"
-  }).then(() => {
-    console.log("added offline pack")
-  }).catch((err: string) => {
-    console.log("err", err)
-  });
-}
+// function setupDownloadPack() {
+//   // Start downloading offline maps
+//   Mapbox.addOfflinePack({
+//     name: 'base',
+//     type: 'bbox',
+//     bounds: [
+//       -37.845485, 140.913340, -28.126389, 153.402606
+//     ],
+//     minZoomLevel: 5,
+//     maxZoomLevel: 10,
+//     // TODO: Get styleURL from configuration
+//     styleURL: "mapbox://styles/mapbox/outdoors-v10"
+//   }).then(() => {
+//     console.log("added offline pack")
+//   }).catch((err: string) => {
+//     console.log("err", err)
+//   });
+// }
 
-function progress(p: DownloadProgress): number {
-  return p.countOfResourcesCompleted / p.countOfResourcesExpected
-}
+// function progress(p: DownloadProgress): number {
+//   return p.countOfResourcesCompleted / p.countOfResourcesExpected
+// }
 
-function setupUpdateProgressCallback(updateProgress: (progress: number) => void) {
-  Mapbox.addOfflinePackProgressListener((p: DownloadProgress) => updateProgress(progress(p)))
-}
+// function setupUpdateProgressCallback(updateProgress: (progress: number) => void) {
+//   Mapbox.addOfflinePackProgressListener((p: DownloadProgress) => updateProgress(progress(p)))
+// }
